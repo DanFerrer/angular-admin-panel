ac4ae255d8541ea02928b0e62370bb0b64ee52ed
DanFerrer <danielferrer212@gmail.com>
2016-11-26 20:33:02 -0500

    removed api middleware abstraction

--- app/components/devices/devices.service.js
+++ app/components/devices/devices.service.js
@@ -1,1 +1,30 @@
-import API_BASE_URL from '../app/constants.js';
+import { API_BASE_URL } from './constants.js';
+
+class DeviceService {
+	constructor($http, ApiMiddleware) {
+		"ngInject;"
+		this.$http = $http;
+	}
+
+	fetchDevices() {
+		return this.$http.get(`${API_BASE_URL}/devices`).then((response) => response.data);
+	}
+
+	fetchDeviceCount() {
+		return this.$http.get(`${API_BASE_URL}/devices/count`).then((response) => response.data);
+	}
+
+	fetchDevice(id) {
+		return this.$http.get(`${API_BASE_URL}/devices/${id}`).then((response) => response.data);
+	}
+
+	createDevice(obj) {
+		return this.$http.post(`${API_BASE_URL}/devices`, obj).then((response) => response.data);
+	}
+
+	deleteDevice(id) {
+		return this.$http.delete(`${API_BASE_URL}/devices/${id}`).then((response) => response.data);
+	}
+};
+
+export default DeviceService;
