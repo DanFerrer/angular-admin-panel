16f49fdb110ce6cd5f43bc2b8fdb43e3bac06426
DanFerrer <danielferrer212@gmail.com>
2016-11-27 23:33:42 -0500

    added jquery :triumph:

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 
 import './app.css';
+import 'materialize-css/node_modules/jquery/dist/jquery.js';
 import 'font-awesome/css/font-awesome.css';
 import 'materialize-css/bin/materialize.css';
 import 'materialize-css/bin/materialize.js';
