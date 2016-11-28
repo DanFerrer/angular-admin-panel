04cc595ba53f3de933f825577df14e4bdfc17185
DanFerrer <danielferrer212@gmail.com>
2016-11-27 21:24:42 -0500

    replaced spaces

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,45 @@
 require('dotenv').config();
 
 module.exports = {
-    devtool: 'source-map',
-    output: {
-    	filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-        	 {
-            	test: /\.html$/,
-            	loader: 'html'
-            },
-            {
-            	test: /\.css$/,
-            	loaders: ['style', 'css']
-            },
-            {
-            	test: /\.js$/,
-            	loaders: ['ng-annotate', 'babel'],
-            	exclude: /node_modules/
-            },
-            {
-      			test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-      			loader: "url?limit=10000&mimetype=application/font-woff"
-    		},
-    		{
-      			test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-     			loader: "url?limit=10000&mimetype=application/font-woff"
-    		},
-    		{
-      			test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-      			loader: "url?limit=10000&mimetype=application/octet-stream"
-    		},
-    		{
-      			test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-      			loader: "file"
-    		},
-    		{
-      			test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-      			loader: "url?limit=10000&mimetype=image/svg+xml"
-    		}
-        ]
-    }
+	devtool: 'source-map',
+	output: {
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.html$/,
+				loader: 'html'
+			},
+			{
+				test: /\.css$/,
+				loaders: ['style', 'css']
+			},
+			{
+				test: /\.js$/,
+				loaders: ['ng-annotate', 'babel'],
+				exclude: /node_modules/
+			},
+			{
+				test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'url?limit=10000&mimetype=application/font-woff'
+			},
+			{
+				test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'url?limit=10000&mimetype=application/font-woff'
+			},
+			{
+				test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'url?limit=10000&mimetype=application/octet-stream'
+			},
+			{
+				test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'file'
+			},
+			{
+				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'url?limit=10000&mimetype=image/svg+xml'
+			}
+		]
+	}
 };
